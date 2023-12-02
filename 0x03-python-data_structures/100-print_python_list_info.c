81d0852cd89315a201119b8c371f1e3c95a3efe2
khloud54 <kh.t705042000@gmail.com>
2023-12-02 02:12:16 -0800

    kk

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,15 @@
+#include <python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = pyList_size(p);
+	int i;
+	pyListObject *obj = (pyListObject *)p;
+
+	printf("[*] size of the python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i ++)
+		printf("Element %i: %s\n", i, py_TYPE(obj->ob_item[i])->tp_name);
+}
