df44a551a4db81e3039dd6083e26fe6aff427490
khloud54 <kh.t705042000@gmail.com>
2023-12-02 00:42:15 -0800

    kk

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
-    for submatrix in matrix:
-        if len(submatrix) == 0:
-            print()
-        for i in range(len(submatrix)):
-            print("{:d}".format(submatrix[i]),
-                    end="\n" if i is len(submatrix) - 1 else " ")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print("")
+    else:
+        for arr in matrix:
+            for m in arr:
+                if m != arr[-1]:
+                    print("{:d}".format(m), end=" ")
+                else:
+                    print("{:d}".format(m))
