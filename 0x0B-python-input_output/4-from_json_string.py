0606d20c7c673d09decb901dd5946fa6d4fc7346
khloud54 <kh.t705042000@gmail.com>
2024-01-09 01:09:45 -0800

    ol

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""defining from_json_string function"""
 
+"""Defines a JSON-to-object function."""
 import json
 
+
 def from_json_string(my_str):
-    """return json string"""
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
