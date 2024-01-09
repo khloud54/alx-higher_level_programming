dabd3b1885f82ca0ae1f6f87c8cee23beaed587a
khloud54 <kh.t705042000@gmail.com>
2024-01-09 00:32:55 -0800

    fk

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+"""Defines read_file function."""
+
 
 def read_file(filename=""):
     """reads filename with utf-8"""
-    with open(filename, encoding='UTF-8') as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
